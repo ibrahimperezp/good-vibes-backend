0a7acb509c27dd5b17aa965e14529f836e88c983
Ibrahim Pérez <ibrahimperezp@gmail.com>
2025-04-04 00:31:55 -0500

    initial storage

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors'
 
 import studentRoutes from './routes/student.routes'
 import teacherRoutes from './routes/teacher.routes'
-import Authentication from './modules/Authentication'
 
 const app = express()
 
@@ -15,14 +14,6 @@ app.use(cors())
 
 app.use('/student', studentRoutes)
 app.use('/teacher', teacherRoutes)
-
-app.post('/user', async (req, res) => {
-  try {
-    const r = await Authentication.addUser('ibrahim', 'ibrahimperezp@gmail.com', 'goodvibes')
-    console.log(r)
-    res.send({ success: true })
-  } catch (e) { res.send({ success: false }) }
-})
 
 app.get('/test', async (req, res) => {
   try {
---
+++ src/modules/Storage.ts
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary'
+import { Response } from '../types.js'
+import { config } from 'dotenv'
+config()
+
+const {
+  CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_SECRET,
+  CLOUDINARY_API_KEY
+} = process.env
+
+class Storage {
+  async saveImage (stringImage: string): Promise<Response> {
+    try {
+      const buffer = Buffer.from(stringImage)
+
+      cloudinary.config({
+        cloud_name: CLOUDINARY_CLOUD_NAME,
+        api_key: CLOUDINARY_API_KEY,
+        api_secret: CLOUDINARY_API_SECRET
+      })
+
+      const response = await new Promise((resolve, reject) => {
+        cloudinary.uploader.upload_stream({ overwrite: false, format: 'webp', folder: 'user' }, (error, result) => {
+          if (error != null) reject(error)
+          resolve(result)
+        }).end(buffer)
+      })
+
+      return { success: true, data: [{ response }] }
+    } catch (e) {
+      return { success: false, errorDescription: 'No se pudo' }
+    }
+  }
+}
+
+export default new Storage()
