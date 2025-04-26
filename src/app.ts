9ca2aa456e354cdae774619b60bceeed1041a991
Ibrahim Pérez <ibrahimperezp@gmail.com>
2025-04-26 17:13:38 -0500

    router version

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
-import router from './v1/routes/index.routes'
+import routerV1 from './v1/routes/index.routes'
 import { API_VERSION_PATH } from './v1/static/paths'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json({ limit: '10mb' }))
-app.use(API_VERSION_PATH.v1, router)
+app.use(API_VERSION_PATH.v1, routerV1)
 
 app.get('/test', async (req, res) => {
   try {
